e8896a1681fa222d0de3fc937352a0622a31723b
Kaybangz <devcaleb96@gmail.com>
2023-06-27 11:24:16 +0100

    Added validate user method in authentication service

--- BallBuddies.Services/Implementation/AuthenticationService.cs
+++ BallBuddies.Services/Implementation/AuthenticationService.cs
@@ -4,7 +4,10 @@ using BallBuddies.Models.Entities;
 using BallBuddies.Services.Interface;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
-
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BallBuddies.Services.Implementation
 {
@@ -15,6 +18,8 @@ namespace BallBuddies.Services.Implementation
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
 
+        private User? _user;
+
         public AuthenticationService(ILoggerManager logger,
             IMapper mapper,
             UserManager<User> userManager,
@@ -24,6 +29,15 @@ namespace BallBuddies.Services.Implementation
             _mapper = mapper;
             _userManager = userManager;
             _configuration = configuration;
+        }
+
+        public async Task<string> CreateToken()
+        {
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
         }
 
         public async Task<IdentityResult> RegisterUser(UserRegistrationDto userRegistrationDto)
@@ -39,6 +53,66 @@ namespace BallBuddies.Services.Implementation
 
             return result;
 
+        }
+
+        public async Task<bool> ValidateUser(UserAuthenticationDto userAuth)
+        {
+            _user = await _userManager.FindByNameAsync(userAuth.UserName);
+
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userAuth.Password));
+
+            if (!result)
+                _logger.LogWarn($"{nameof(ValidateUser)}: Authentication failed. \nWrong username or password.");
+
+            return result;
+        }
+
+
+        private SigningCredentials GetSigningCredentials()
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET"));
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            var roles = await _userManager.GetRolesAsync(_user);
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials,
+            List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("JwtSettings");
+
+            var tokenOptions = new JwtSecurityToken
+                (
+                    issuer: jwtSettings["validIssuer"],
+                    audience: jwtSettings["validAudience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+                    signingCredentials: signingCredentials
+                );
+
+            return tokenOptions;
         }
     }
 }
--- BallBuddies.Services/Interface/IAuthenticationService.cs
+++ BallBuddies.Services/Interface/IAuthenticationService.cs
@@ -6,5 +6,7 @@ namespace BallBuddies.Services.Interface
     public interface IAuthenticationService
     {
         Task<IdentityResult> RegisterUser(UserRegistrationDto userRegistrationDto);
+        Task<bool> ValidateUser(UserAuthenticationDto userAuth);
+        Task<string> CreateToken();
     }
 }
